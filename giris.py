40d80c5d414a48d3b62d59574dce77dea7b4c64c
Arda <ardacicek2011@gmail.com>
2020-05-30 02:40:32 +0300

    Aidat ekleme özelliği eklendi. veritabanında değişiklik yapıldı. her kata özel örnekler oluşturuldu ve aidatlar sözlük biçiminde tanımlandı

--- giris.py
+++ giris.py
@@ -6,18 +6,75 @@ vt = sql.connect("admin.sqlite")
 cs = vt.cursor()
 cs.execute("SELECT * FROM admin WHERE parola =(?)",(parola,))
 admin_bilgisi = cs.fetchone()
-if parola in admin_bilgisi:
-    class AdminPanel:
-        def __init__(self):
-            pass
-    admin = AdminPanel()
-    emir = input("Yönetici Hesabıyla Giriş Yaptınız\nLütfen Yapmak İstediğiniz İşlemi Giriniz\n>> ")
-elif parola in kayit.Uye.parolalar:
-    class UyePanel:
-        def __init__(self):
-            pass
-    uye = UyePanel()
-    emir = input("Üye Hesabıyla Giriş Yaptınız\nLütfen Yapmak İstediğiniz İşlemi Giriniz\n>> ")
+if admin_bilgisi == None:
+    vt = sql.connect("admin.sqlite")
+    cs = vt.cursor()
+    cs.execute("SELECT * FROM admin WHERE parola =(?)", (parola,))
+    admin_bilgisi = cs.fetchone()
+    print("Bir Hata Oluştu Lütfen Tekrar Deneyiniz")
 else:
-    print("Girdiğiniz parola hatalı!")
-    cikis()
+    if parola in admin_bilgisi:
+        class AdminPanel:
+            def __init__(self):
+                pass
+            def aidatEkle(self,ay="",miktar=0,kat=1,daire=1):
+                if kat == "1" and daire == "1":
+                    kayit.kat1daire1.aidat[ay] = miktar
+                elif kat == "1" and daire == "2":
+                    kayit.kat1daire2.aidat[ay] = miktar
+                elif kat == "1" and daire == "3":
+                    kayit.kat1daire3.aidat[ay] = miktar
+                elif kat == "2" and daire == "4":
+                    kayit.kat2daire4.aidat[ay] = miktar
+                elif kat == "2" and daire == "5":
+                    kayit.kat2daire5.aidat[ay] = miktar
+                elif kat == "2" and daire == "6":
+                    kayit.kat2daire6.aidat[ay] = miktar
+                elif kat == "3" and daire == "7":
+                    kayit.kat3daire7.aidat[ay] = miktar
+                elif kat == "3" and daire == "8":
+                    kayit.kat3daire8.aidat[ay] = miktar
+                elif kat == "3" and daire == "9":
+                    kayit.kat3daire9.aidat[ay] = miktar
+                elif kat == "4" and daire == "10":
+                    kayit.kat4daire10.aidat[ay] = miktar
+                elif kat == "4" and daire == "11":
+                    kayit.kat4daire11.aidat[ay] = miktar
+                elif kat == "4" and daire == "12":
+                    kayit.kat4daire12.aidat[ay] = miktar
+                elif kat == "5" and daire == "13":
+                    kayit.kat5daire13.aidat[ay] = miktar
+                elif kat == "5" and daire == "14":
+                    kayit.kat5daire14.aidat[ay] = miktar
+                elif kat == "5" and daire == "15":
+                    kayit.kat5daire15.aidat[ay] = miktar
+                elif kat == "6" and daire == "16":
+                    kayit.kat6daire16.aidat[ay] = miktar
+                else:
+                    print("Yanlış Bilgiler Girildi")
+
+        admin = AdminPanel()
+        emir = input("Yönetici Hesabıyla Giriş Yaptınız\nLütfen Yapmak İstediğiniz İşlemi Giriniz\n>> ")
+        if emir == "1":
+            kat = input("kat:")
+            daire = input("daire:")
+            ay = input("ay:")
+            miktar = input("miktar:")
+            admin.aidatEkle(ay,miktar,kat,daire)
+            print("Aidat Başarılı Bir Şekilde Eklendi")
+
+    elif parola in kayit.Uye.parolalar:
+        class UyePanel:
+            def __init__(self):
+                pass
+            def aidatGoruntule(self,kat=1,daire=1):
+                print("Fatura")
+        uye = UyePanel()
+        emir = input("Üye Hesabıyla Giriş Yaptınız\nLütfen Yapmak İstediğiniz İşlemi Giriniz\n>> ")
+        if emir == "1":
+            kat = input("kat:")
+            daire = input("daire:")
+            uye.aidatGoruntule(kat,daire)
+    else:
+        print("Girdiğiniz parola hatalı!")
+        cikis()
--- kayit.py
+++ kayit.py
@@ -4,9 +4,10 @@ class Uye:
     parolalar = list()
     __isimler = list()
     _isim = list()
-    def __init__(self,isim = ""):
+    def __init__(self,isim = "",soyisim = ""):
         self.isim = isim.lower()
-        self.fatura = 0
+        self.soyisim = soyisim
+        self.aidat = dict()
         self.__kayit()
     def kullaniciAdiOlustur(self):
        self._isim.clear()
@@ -17,41 +18,36 @@ class Uye:
         kod = "".join(self._isim)
         if kod not in self.parolalar:
             while len(kod) < 20:
-                kod += 0
+                kod += "0"
             self.parolalar.append(kod[:20])
             self.__isimler.append(self.isim)
             self.__veritabaninaKaydet()
     def __veritabaninaKaydet(self):
         vt = sql.connect("users.sqlite")
         cs = vt.cursor()
-        cs.execute("CREATE TABLE IF NOT EXISTS users (isim,parola,aidat)")
+        cs.execute("CREATE TABLE IF NOT EXISTS users (isim,parola)")
         cs.execute("SELECT * from users")
         bilgiler = cs.fetchone()
         try:
             if self.isim not in bilgiler:
-                veri = (self.isim, self.parolalar[self.__isimler.index(self.isim)], self.fatura)
-                cs.execute("INSERT INTO users values (?,?,?)", veri)
+                veri = (self.isim, self.parolalar[self.__isimler.index(self.isim)])
+                cs.execute("INSERT INTO users values (?,?)", veri)
                 vt.commit()
                 vt.close()
-                print("Üye Başarıyla Eklendi")
-                print("Üye Parolanız:",self.parolalar[self.__isimler.index(self.isim)])
-                print("-"*100)
             else:
                 vt.close()
         except:
-            veri = (self.isim, self.parolalar[self.__isimler.index(self.isim)],self.fatura)
-            cs.execute("INSERT INTO users values (?,?,?)", veri)
+            veri = (self.isim, self.parolalar[self.__isimler.index(self.isim)])
+            cs.execute("INSERT INTO users values (?,?)", veri)
             vt.commit()
-            print("Üye Başarıyla Eklendi")
-            print("Üye Parolanız:",self.parolalar[self.__isimler.index(self.isim)])
-            print("-"*100)
             vt.close()
 
 class Yonetici:
     admin = list()
     admin_name = list()
-    def __init__(self,isim=""):
+    def __init__(self,isim="",soyisim=""):
         self.isim = isim.lower()
+        self.soyisim = soyisim
         self.YoneticiEkle()
     def YoneticiEkle(self):
         self.admin_name.clear()
@@ -66,27 +62,21 @@ class Yonetici:
         cs.execute("INSERT INTO admin VALUES (?,?)",(self.isim,self.admin[:20]))
         vt.commit()
         vt.close()
-        print("Yönetici Başarıyla Eklendi")
-        print("Yönetici Parolanız:", self.admin[:20])
-        print("-" * 100)
 
-if __name__ == "__main__":
-    while True:
-        islem = input("Yapmak istediğiniz işlemi giriniz\nYönetici eklemek için 1\nÜye eklemek için 2\nÇıkmak için q yazınız\n>> ")
-        if islem == "1":
-            isim = input("Yöneticinin ismi: ")
-            sleep(0.5)
-            Yonetici(isim)
-            print()
-        elif islem == "2":
-            isim = input("Üyenin ismi: ")
-            sleep(0.5)
-            Uye(isim)
-            print()
-        elif islem == "q":
-            print("Hoşçakal...")
-            sleep(1.5)
-            break
-        else:
-            print("Yanlış Giriş Yapıldı")
-            break
+# Apartman Kaydı Oluşturuluyor #
+kat1daire1 = Uye("Kemal")
+kat1daire2 = Uye("Mehmet")
+kat1daire3 = Uye("Mustafa")
+kat2daire4 = Uye("Abdullah")
+kat2daire5 = Uye("Hakan")
+kat2daire6 = Uye("Goktug")
+kat3daire7 = Uye("Recep")
+kat3daire8 = Yonetici(isim="Kamil",soyisim="Çiçek")
+kat3daire9 = Uye("Arda")
+kat4daire10 = Uye("Ali")
+kat4daire11 = Uye("Ozlem")
+kat4daire12 = Uye("Celal")
+kat5daire13 = Uye("Berk")
+kat5daire14 = Uye("Buğra")
+kat5daire15 = Uye("Muhammed")
+kat6daire16 = Uye("Serdar")
