6278782f7567b4f39baf20ef766939bebd16e21a
Arda <ardacicek2011@gmail.com>
2020-05-29 23:04:25 +0300

    Giriş kısmı için dosya oluşturuldu. Yönetim paneli için başlangıç kodları yazıldı

--- users.py
+++ users.py
@@ -1,42 +1,41 @@
 import sqlite3 as sql
+class Kayit:
+    __uyeler = list()
+    __isimler = list()
+    _isim = list()
 
-class User:
-    __uyeler = []
-    __isimler = []
     def __init__(self,isim = ""):
-        self.isim = isim
-        self.__kayit()
-
+        self.isim = isim.lower()
+        self.fatura = 0
+    def kullaniciAdiOlustur(self):
+       self._isim.clear()
+       for harf in self.isim:
+           self._isim.append(str(ord(harf)))
     def __kayit(self):
-        _isim = []
-        for harf in self.isim:
-            _isim.append(str(ord(harf)))
-        self.__uyeler.append("".join(_isim))
-        self.__isimler.append(self.isim)
-        self.__veritabaninaKaydet()
-
+        self.kullaniciAdiOlustur()
+        if "".join(self._isim) not in self.__uyeler:
+            self.__uyeler.append("".join(self._isim))
+            self.__isimler.append(self.isim)
+            self.__veritabaninaKaydet()
     def __veritabaninaKaydet(self):
         vt = sql.connect("users.sqlite")
         cs = vt.cursor()
-        cs.execute("CREATE TABLE IF NOT EXISTS users (isim,username)")
+        cs.execute("CREATE TABLE IF NOT EXISTS users (isim,username,aidat)")
         cs.execute("SELECT * from users")
         bilgiler = cs.fetchone()
         try:
             if self.isim not in bilgiler:
-                veri = (self.isim,self.__uyeler[self.__isimler.index(self.isim)])
-                cs.execute("INSERT INTO users values (?,?)",veri)
+                veri = (self.isim, self.__uyeler[self.__isimler.index(self.isim)], self.fatura)
+                cs.execute("INSERT INTO users values (?,?,?)", veri)
                 vt.commit()
                 vt.close()
                 print("Kayıt Edildi")
             else:
                 vt.close()
         except:
-            veri = (self.isim, self.__uyeler[self.__isimler.index(self.isim)])
-            cs.execute("INSERT INTO users values (?,?)", veri)
+            veri = (self.isim, self.__uyeler[self.__isimler.index(self.isim)],self.fatura)
+            cs.execute("INSERT INTO users values (?,?,?)", veri)
             vt.commit()
-            vt.close()
             print("Kayıt Edildi")
             vt.close()
-    @classmethod
-    def uyeleriGoruntule(cls):
-        return cls.__uyeler
+
