4b084ac1b5a3962668d267ed70a5e521cb6553c5
Arda <ardacicek2011@gmail.com>
2020-05-29 22:05:06 +0300

    gereksiz dosyalar silindi kayıt işlemi için gerekli veritabanı kodları yazıldı

--- users.py
+++ users.py
@@ -1,5 +1,8 @@
-class Yonetim:
+import sqlite3 as sql
+
+class User:
     __uyeler = []
+    __isimler = []
     def __init__(self,isim = ""):
         self.isim = isim
         self.__kayit()
@@ -9,9 +12,31 @@ class Yonetim:
         for harf in self.isim:
             _isim.append(str(ord(harf)))
         self.__uyeler.append("".join(_isim))
-        print("Kayıt Yapıldı")
+        self.__isimler.append(self.isim)
+        self.__veritabaninaKaydet()
 
+    def __veritabaninaKaydet(self):
+        vt = sql.connect("users.sqlite")
+        cs = vt.cursor()
+        cs.execute("CREATE TABLE IF NOT EXISTS users (isim,username)")
+        cs.execute("SELECT * from users")
+        bilgiler = cs.fetchone()
+        try:
+            if self.isim not in bilgiler:
+                veri = (self.isim,self.__uyeler[self.__isimler.index(self.isim)])
+                cs.execute("INSERT INTO users values (?,?)",veri)
+                vt.commit()
+                vt.close()
+                print("Kayıt Edildi")
+            else:
+                vt.close()
+        except:
+            veri = (self.isim, self.__uyeler[self.__isimler.index(self.isim)])
+            cs.execute("INSERT INTO users values (?,?)", veri)
+            vt.commit()
+            vt.close()
+            print("Kayıt Edildi")
+            vt.close()
     @classmethod
     def uyeleriGoruntule(cls):
         return cls.__uyeler
-
